c33eda550d489b95e33248543820e11447ff4412
James Thorne <james@jthorne.co.uk>
2016-05-19 14:05:35 +0100

    made common objects extend layer

--- src/main/java/gpig/group2/maps/objective/ObjectivesLayer.java
+++ src/main/java/gpig/group2/maps/objective/ObjectivesLayer.java
@@ -1,7 +1,9 @@
 package gpig.group2.maps.objective;
 
+import gpig.group2.maps.common.Layer;
+
 /**
  * Created by james on 19/05/2016.
  */
-public abstract class ObjectivesLayer {
+public abstract class ObjectivesLayer extends Layer {
 }
--- src/main/java/gpig/group2/maps/platform/PositionLayer.java
+++ src/main/java/gpig/group2/maps/platform/PositionLayer.java
@@ -1,7 +1,9 @@
 package gpig.group2.maps.platform;
 
+import gpig.group2.maps.common.Layer;
+
 /**
  * Created by james on 19/05/2016.
  */
-public class PositionLayer {
+public class PositionLayer extends Layer {
 }
