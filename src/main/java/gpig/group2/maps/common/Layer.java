309629623175a891c36fde5abf0b0254513b25c0
James Thorne <james@jthorne.co.uk>
2016-05-19 11:54:12 +0100

    Common geo objects

---
+++ src/main/java/gpig/group2/maps/common/Layer.java
@@ -0,0 +1,12 @@
+package gpig.group2.maps.common;
+
+import gpig.group2.maps.geographic.GeographicObject;
+
+import java.util.Set;
+
+/**
+ * Created by james on 19/05/2016.
+ */
+public class Layer {
+    Set<GeographicObject> objects;
+}
---
+++ src/main/java/gpig/group2/maps/geographic/Position.java
@@ -0,0 +1,10 @@
+package gpig.group2.maps.geographic;
+
+/**
+ * Created by james on 19/05/2016.
+ */
+public class Position {
+    private float latitude;
+    private float longitude;
+
+}
