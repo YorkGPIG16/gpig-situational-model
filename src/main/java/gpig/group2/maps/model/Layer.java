19c3f98eb94eaf7012e35977a6be0347acfd8166
James Thorne <james@jthorne.co.uk>
2016-05-19 10:42:31 +0100

    New project

---
+++ src/main/java/gpig/group2/maps/model/Layer.java
@@ -0,0 +1,9 @@
+package gpig.group2.maps.model;
+
+/**
+ * Created by james on 19/05/2016.
+ */
+public class Layer {
+    public Integer id;
+    public String name;
+}
---
+++ src/main/java/gpig/group2/maps/model/LayerGroup.java
@@ -0,0 +1,7 @@
+package gpig.group2.maps.model;
+
+/**
+ * Created by james on 19/05/2016.
+ */
+public class LayerGroup {
+}
