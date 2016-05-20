7434e3b21aefd8b2a2dc850473c41a1cb390c2c8
oloftus <o@oloft.us>
2016-05-20 15:37:05 +0100

    Added getters and setters

--- src/main/java/gpig/group2/maps/platform/Drone.java
+++ src/main/java/gpig/group2/maps/platform/Drone.java
@@ -1,43 +1,59 @@
 package gpig.group2.maps.platform;
 
-import gpig.group2.maps.geographic.Coordinate;
-import gpig.group2.maps.geographic.position.CoordinateList;
-import gpig.group2.maps.geographic.position.Point;
-import gpig.group2.maps.objective.ObjectivesLayer;
-
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
-/**
- * Created by james on 19/05/2016.
- */
+import gpig.group2.maps.geographic.position.CoordinateList;
+import gpig.group2.maps.geographic.position.Point;
+
 public class Drone implements DroneUpdating {
 
-    Set<GivesObjectives> subscribesToObjectives;
+	Set<GivesObjectives> subscribesToObjectives;
 
+	private CoordinateList waypoints;
+	private CoordinateList history;
+	private Point position;
 
-    CoordinateList waypoints;
-    CoordinateList history;
-    Point position;
+	public Drone() {
+		subscribesToObjectives = new HashSet<>();
 
+		waypoints = new CoordinateList();
+		history = new CoordinateList();
+		position = new Point();
+	}
 
-    public Drone() {
-        subscribesToObjectives = new HashSet<>();
+	public void addSubscriptionToObjective(GivesObjectives layer) {
+		subscribesToObjectives.add(layer);
+	}
 
-        waypoints = new CoordinateList();
-        history = new CoordinateList();
-        position = new Point();
-    }
+	@Override
+	public void updateLocation(Point newPosition) {
+		history.addCoordinate(position);
+		position = newPosition;
 
-    public void addSubscriptionToObjective(GivesObjectives layer) {
-        subscribesToObjectives.add(layer);
-    }
+	}
 
-    @Override
-    public void updateLocation(Point newPosition) {
-        history.addCoordinate(position);
-        position = newPosition;
+	public CoordinateList getWaypoints() {
+		return waypoints;
+	}
 
-    }
+	public void setWaypoints(CoordinateList waypoints) {
+		this.waypoints = waypoints;
+	}
+
+	public CoordinateList getHistory() {
+		return history;
+	}
+
+	public void setHistory(CoordinateList history) {
+		this.history = history;
+	}
+
+	public Point getPosition() {
+		return position;
+	}
+
+	public void setPosition(Point position) {
+		this.position = position;
+	}
 }
